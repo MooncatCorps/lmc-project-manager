74c2c17964d661f10ec71a0add896da1a41d4029
AyimanyMooncat <mooncatayimany@proton.me>
2024-06-22 21:52:08 -0600

    [MOD] Create generic error messages

--- modules/errors.py
+++ modules/errors.py
@@ -1,4 +1,5 @@
 import logging
+from pathlib import Path
 
 def tripartite_error(what: str, why: str, cause: str, logger: logging.Logger):
     logger.error(what)
@@ -16,3 +17,23 @@ def misconfigured_settings(keys: list[str], logger: logging.Logger):
 
 def file_permissions(keys: list[str], logger: logging.Logger):
     settings_issue(keys, "Missing File Permissions", logger)
+
+def file_not_found(path: Path, logger: logging.Logger):
+    logger.error("File not found")
+    logger.error(f"Could not locate {path}")
+    logger.info("Check for filesystem permissionsor missing files")
+
+def insufficient_file_permissions(path: Path, keys: list[str], logger: logging.Logger):
+    logger.error("Insufficient File Permissions")
+    logger.error(f"Cannot operate on path {path}")
+    logger.error("Check your filesystem permissions")
+    file_permissions(keys, logger)
+
+def misconfiguration(what_it_caused: str, settings_with_their_whys: dict[str, str], logger: logging.Logger):
+    logger.error("Misconfiguration")
+    logger.error("An error ocurred due to misconfigured settings in .mooncat.toml")
+    logger.error(what_it_caused)
+
+    logger.info("Problematic settings and why they matter:")
+    for setting, reason in settings_with_their_whys:
+        logger.info(f"Problem: {setting} -> {reason}")
--- modules/installers.py
+++ modules/installers.py
@@ -13,44 +13,36 @@ def install_library_files(name: str, lang: str, logger: logging.Logger, dev: boo
     logger.debug(f"Base Destination Path: {path_base}")
 
     if path_base == None:
-        errors.tripartite_error(
-            "Failed to install library files",
-            "Could not determine destination (to which files should be installed)",
-            "This is usually caused by a misconfigured language name",
-            logger)
-
-        errors.misconfigured_settings(["development.lang"], logger)
-
+        errors.misconfiguration("Could not determine library install path",
+                                { "development.lang" : "Helps determine installation destination directory"},
+                                logger)
         return False
 
-    if not paths.has_access(path_base):
-        errors.tripartite_error(
-            "Failed to install library files",
-            f"Cannot operate on destination: {path_base}",
-            "Do you have the right file permissions?",
-            logger)
 
-        errors.file_permissions(["read", "write", "execute"], logger)
+    if not path_base.exists():
+        logger.error("Missing library install location")
+        errors.file_not_found(path_base, logger)
+        return False
+
+
+    if not paths.has_access(path_base):
+        errors.insufficient_file_permissions(path_base, ["read", "write", "execute"], logger)
         return False
 
     if path_from == None:
-        errors.tripartite_error(
-            "Failed to install library files",
-            "Could not determine source (from which to install files)",
-            "This is usually caused by a misconfigured language name",
-            logger)
+        errors.misconfiguration("Could not determine library install source",
+                                { "development.lang" : "Helps determine installation source directory"},
+                                logger)
+        return False
 
-        errors.misconfigured_settings(["development.lang"], logger)
+    if not path_from.exists():
+        logger.error("Missing library install source")
+        errors.file_not_found(path_base, logger)
+        logger.info(f"Your library file directory should be named {path_from.name}")
         return False
 
     if not paths.can_read(path_from):
-        errors.tripartite_error(
-            "Failed to install library files",
-            f"Cannot read from source: {path_from}",
-            "Do you have the right permissions?",
-            logger)
-
-        errors.file_permissions(["read"], logger)
+        errors.insufficient_file_permissions(path_from, ["read"], logger)
         return False
 
     path_moon = paths.append_mooncat(path_base)
@@ -60,13 +52,7 @@ def install_library_files(name: str, lang: str, logger: logging.Logger, dev: boo
     logger.debug(f"Library Destination Path: {path_lib}")
 
     if not paths.has_access(path_moon):
-        errors.tripartite_error(
-            "Failed to install library files",
-            f"Cannot operate on destination: {path_moon}",
-            "Do you have the right file permissions?",
-            logger)
-
-        errors.file_permissions(["read", "write", "execute"], logger)
+        errors.insufficient_file_permissions(path_moon, ["read", "write", "execute"], logger)
         return False
 
     logger.debug(f"Attempting to create path: {path_moon}")
@@ -84,34 +70,50 @@ def install_library_files(name: str, lang: str, logger: logging.Logger, dev: boo
 
 
 def uninstall_library_files(name: str, lang: str, logger: logging.Logger) -> bool:
-    path_dest = paths.external_library_files(lang)
+    logger.debug("Performing library uninstallation")
 
-    if path_dest == None:
-        errors.tripartite_error(
-            "Failed to uninstall library files",
-            "Could not determine target (from which files should be removed)",
-            "This is usually caused by a misconfigured language name",
-            logger)
+    path_base = paths.external_library_files(lang)
 
-        errors.misconfigured_settings(["development.lang"], logger)
+    logger.debug(f"Base Destination Path: {path_base}")
 
+    if path_base == None:
+        errors.misconfiguration("Could not determine library uninstall path",
+                                { "development.lang" : "Helps determine (un)installation destination directory"},
+                                logger)
         return False
 
-    path_dest = paths.append_mooncat(path_dest)
 
-    if not paths.can_delete(path_dest):
-        errors.tripartite_error(
-            "Failed to uninstall library files",
-            f"Cannot operate on destination: ({path_dest})",
-            "Do you have the right file permissions?",
-            logger)
-
-        errors.file_permissions(["write", "execute"], logger)
+    if not path_base.exists():
+        logger.error("Missing library uninstall target")
+        logger.info("This usually means that there is nothing to uninstall")
+        errors.file_not_found(path_base, logger)
         return False
 
-    path_dest /= name
-    if path_dest.exists():
-        paths.remove(path_dest)
 
+    if not paths.has_access(path_base):
+        errors.insufficient_file_permissions(path_base, ["read", "write", "execute"], logger)
+        return False
+
+    path_moon = paths.append_mooncat(path_base)
+    path_lib = path_moon/name
+
+    logger.debug(f"Mooncat Destination Path: {path_moon}")
+    logger.debug(f"Library Destination Path: {path_lib}")
+
+    if not paths.has_access(path_moon):
+        errors.insufficient_file_permissions(path_moon, ["read", "write", "execute"], logger)
+        return False
+
+    logger.debug(f"Attempting to create path: {path_moon}")
+    os.makedirs(path_moon, exist_ok = True)
+
+    if path_lib.exists():
+        logger.debug(f"Cleaning up: {path_base}")
+        paths.remove(path_lib)
+    else:
+        errors.file_not_found(path_lib, logger)
+        logger.info("This usually means that there is nothing to uninstall")
+        return False
+
+    logger.debug(f"Done")
     return True
-
