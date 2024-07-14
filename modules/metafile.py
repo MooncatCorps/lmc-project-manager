6a802586770f38f8bb98ec7f13e11f070a20ddeb
Ayimany <mooncatayimany@proton.me>
2024-07-14 15:39:28 -0600

    [MOD] Move metafile functions [paths.py -> metafile.py]

--- modules/metafile.py
+++ modules/metafile.py
@@ -1,8 +1,15 @@
-import re
-import tomllib
-import logging
-from typing import Optional
 from witherlabs.projman import paths
+from pathlib import Path
+import tomllib
+
+
+LAB_NAMESPACE = 'witherlabs'
+METAFILE_NAME = '.wither'
+
+
+def path() -> Path:
+    return paths.cwd()/METAFILE_NAME
+
 
 
 def exists() -> bool:
@@ -38,64 +45,4 @@ def create() -> bool:
 def parse() -> dict:
     with open(paths.metafile(), 'rb') as metafile:
         return tomllib.load(metafile)
-
-
-def interactive_create(logger: logging.Logger) -> bool:
-
-    if not exists():
-        logger.warn(f'Missing {paths.METAFILE_NAME}')
-
-        if not input('Create it now? [Y/n]').lower() in [ '', 'y', 'yes' ]:
-            return False
-
-        if not paths.can_write(paths.cwd()):
-            logger.error(f'Insufficient permissions to create {paths.metafile()}')
-            return False
-
-        create()
-
-    if is_invalid():
-        logger.error(f'{paths.METAFILE_NAME} exists, but is not a regular file.')
-        logger.info(f'{paths.METAFILE_NAME} is a {paths.get_file_entity_name(paths.metafile())}')
-        return False
-
-    return True
-
-
-def interactive_parse(logger: logging.Logger) -> Optional[dict]:
-    val = None
-
-    if not interactive_create(logger):
-        return val
-
-    content = ''
-
-    with open(paths.metafile(), 'r') as metafile:
-        content = metafile.read()
-
-    try:
-        val = tomllib.loads(content)
-    except tomllib.TOMLDecodeError as err:
-        reason = str(err)
-        logger.error(f'The {paths.METAFILE_NAME} file contains errors')
-        logger.error(f'Reason: {reason}')
-
-        match = re.search(r'\(at line (.*), column (.*)\)', reason)
-
-        if not match:
-            return val
-
-        str_line = match.groups()[0]
-        str_column = match.groups()[1]
-
-        if not str_line.isnumeric() or not str_column.isnumeric():
-            return val
-
-        line = int(str_line)
-        column = int(str_column)
-
-        lnstr = content.split('\n')[line - 1]
-        logger.info(f'{paths.METAFILE_NAME}:\n{line}:\t{lnstr}\n{' ' * len(str_line)} \t{' ' * (column - 2)}^^^\n')
-
-    return val
 
--- modules/paths.py
+++ modules/paths.py
@@ -1,35 +1,9 @@
-import os, platform, sysconfig, shutil
+import os, shutil
 from pathlib import Path
-from typing import Optional
-
-
-LAB_NAMESPACE = 'witherlabs'
-METAFILE_NAME = '.wither'
-
-
-#
-# OS
-#
 
 
 def cwd() -> Path:
     return Path(os.getcwd())
-
-
-def can_read(path: Path):
-    return path.exists() and os.access(path, os.R_OK)
-
-
-def can_write(path: Path):
-    return path.exists() and os.access(path, os.W_OK)
-
-
-def can_delete(path: Path):
-    return path.exists() and os.access(path, os.W_OK | os.X_OK)
-
-
-def has_access(path: Path):
-    return path.exists() and os.access(path, os.W_OK | os.X_OK | os.R_OK)
 
 
 def get_file_entity_name(path: Path):
@@ -65,46 +39,14 @@ def remove(path: Path):
         shutil.rmtree(path)
 
 
-def copy(path_from: Path, path_dest: Path, symlink: bool = False):
-    if symlink:
-        os.symlink(path_from, path_dest)
+def copy(path_from: Path, path_dest: Path):
+    if path_from.is_dir():
+        shutil.copytree(path_from, path_dest)
     else:
-        if path_from.is_dir():
-            shutil.copytree(path_from, path_dest)
-        else:
-            shutil.copy(path_from, path_dest)
+        shutil.copy(path_from, path_dest)
 
 
-#
-# Standard Files
-#
-
-
-def metafile() -> Path:
-    return cwd()/METAFILE_NAME
-
-
-#
-# Language Specific: External Paths
-#
-
-
-def internal_library_files(lang: str) -> Optional[Path]:
-    match lang:
-        case 'python':
-            return cwd()/'modules'
-        case 'c' | 'c++':
-            return cwd()/'include'
-
-    return None
-
-
-#
-# Constants
-#
-
-
-def append_namespace(path: Path) -> Path:
-    return path/LAB_NAMESPACE
+def symlink(path_from: Path, path_dest: Path):
+    os.symlink(path_from, path_dest)
 
 
