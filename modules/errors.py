a01977ba94ec04f676d64c68aad9fcb27a49d7e0
Ayimany <mooncatayimany@proton.me>
2024-07-14 15:56:04 -0600

    [ADD] WLMPErrorType to trak errors

--- modules/errors.py
+++ modules/errors.py
@@ -1,47 +1,28 @@
-import logging
+from typing import Optional
 from pathlib import Path
-
-def tripartite_error(what: str, why: str, cause: str, logger: logging.Logger):
-    logger.error(what)
-    logger.error(why)
-    logger.info(cause)
+from enum import Enum
 
 
-def settings_issue(keys: list[str], reason: str, logger: logging.Logger):
-    logger.error(f'{reason}: {', '.join(keys)}')
+class WLPMErrorType(Enum):
+    GENERIC = 1,
+    FILE = 2,
 
 
-def missing_settings(keys: list[str], logger: logging.Logger):
-    settings_issue(keys, 'Missing settings', logger)
+
+class WLPMError:
+
+    def __init__(self, etype: WLPMErrorType, reason: str):
+        self.etype = etype
+        self.reason = reason
 
 
-def misconfigured_settings(keys: list[str], logger: logging.Logger):
-    settings_issue(keys, 'Misconfigured settings', logger)
+class PossibleError[T]:
+
+    def __init__(self, value: Optional[T]):
+        self.value = value
+        self.error_count: int = 0
+        self.errors: list[WLPMError] = []
 
 
-def file_permissions(keys: list[str], logger: logging.Logger):
-    settings_issue(keys, 'Missing File Permissions', logger)
-
-
-def file_not_found(path: Path, logger: logging.Logger):
-    logger.error('File not found')
-    logger.error(f'Could not locate: {path}')
-    logger.info('Check for filesystem permissions or missing files')
-
-
-def insufficient_file_permissions(path: Path, keys: list[str], logger: logging.Logger):
-    logger.error('Insufficient File Permissions')
-    logger.error(f'Cannot operate on path {path}')
-    logger.error('Check your filesystem permissions')
-    file_permissions(keys, logger)
-
-
-def misconfiguration(what_it_caused: str, settings_with_their_whys: dict[str, str], logger: logging.Logger):
-    logger.error('Misconfiguration')
-    logger.error('An error ocurred due to misconfigured settings')
-    logger.error(what_it_caused)
-
-    logger.info('Problematic settings and why they matter:')
-    for setting, reason in settings_with_their_whys:
-        logger.info(f'Problem: {setting} -> {reason}')
-
+def non_existent_path(path: Path):
+    return WLPMError(WLPMErrorType.FILE, f'Path does not exist: {path}')
